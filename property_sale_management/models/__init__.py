dbc65463bd7a6109e8f1ddd17c6ce851a2da30cd
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-05-24 00:10:03 +0400

    bo

--- property_sale_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import property_sale
-from . import property_property
-from . import account_move
-from . import internal_commission
-from . import broker_commission_invoice
-from . import res_partner
-from . import account_type
