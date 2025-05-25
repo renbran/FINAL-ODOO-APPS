a44e63b7d7e16e2c7927031e83ee9b079a20505a
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-05-26 01:24:57 +0400

    gg

--- real_estate_management_v2/__manifest__.py
+++ real_estate_management_v2/__manifest__.py
@@ -8,18 +8,34 @@
     'license': 'LGPL-3',
     'depends': ['base', 'mail', 'account'],
     'data': [
+        # Security
         'security/ir.model.access.csv',
+        
+        # Data
+        'data/ir_sequence_data.xml',
+        
+        # Views
+        'views/menu_views.xml',
         'views/property_views.xml',
         'views/property_offer_views.xml',
         'views/property_sale_views.xml',
+        'views/property_sale_line_views.xml',
         'views/broker_commission_views.xml',
         'views/internal_commission_views.xml',
+        'views/res_config_views.xml',
+        'views/property_action_views.xml',
+        
+        # Wizards
         'wizards/property_report_wizard_views.xml',
-        'data/ir_sequence_data.xml',
     ],
     'demo': [
         'demo/property_demo.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'real_estate_management_v2/static/src/scss/property.scss',
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
