616a9c8a6a7f8c78d15d44dbd042494f06605ad8
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-05-26 15:18:22 +0400

    go

--- property_sale_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import property_property
-from . import property_sale
-from . import broker_commission_invoice
-from . import internal_commission
-from . import internal_commission_purchase
-from . import account_move
-from . import res_partner
--- real_estate_management_v2/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import property_property
-from . import property_offer
-from . import property_sale
-from . import property_sale_line
-from . import broker_commission
-from . import internal_commission
-from . import res_config
