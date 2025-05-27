c1dde1ca7f26a2d4eb6bf0d1a61832545450ad9f
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-05-27 09:36:08 +0400

    g

--- property_management/models/property_property.py
+++ property_management/models/property_property.py
@@ -71,7 +71,7 @@ class PropertyProperty(models.Model):
 
     status = fields.Char(
         string="Status Display", 
-        compute='_compute_status', 
+        compute="_compute_status", 
         store=True
     )
 
@@ -83,28 +83,28 @@ class PropertyProperty(models.Model):
     )
     total_invoiced = fields.Monetary(
         string="Total Invoiced", 
-        compute='_compute_payment_details', 
+        compute="_compute_payment_details", 
         store=True
     )
     total_paid = fields.Monetary(
         string="Total Received", 
-        compute='_compute_payment_details', 
+        compute="_compute_payment_details", 
         store=True
     )
     remaining_amount = fields.Monetary(
         string="Pending Amount", 
-        compute='_compute_payment_details', 
+        compute="_compute_payment_details", 
         store=True
     )
 
     # Smart Counters
     offer_count = fields.Integer(
         string="Offers Count", 
-        compute='_compute_offer_count'
+        compute="_compute_offer_count"
     )
     sale_count = fields.Integer(
         string="Sales Count", 
-        compute='_compute_sale_count'
+        compute="_compute_sale_count"
     )
 
     # Accounting
@@ -122,12 +122,12 @@ class PropertyProperty(models.Model):
     # Project Information
     project_name = fields.Char(
         string="Project Name", 
-        default='Sky Hills Astra'
+        default="Sky Hills Astra"
     )
     active_sale_id = fields.Many2one(
         'property.sale', 
         string="Active Sale", 
-        compute='compute_active_sale'
+        compute="_compute_active_sale"
     )
     broker_commission_count = fields.Integer(
     string="Commission Count", 
