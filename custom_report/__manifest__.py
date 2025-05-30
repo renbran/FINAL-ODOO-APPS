a5ffc98a6d3ecfbaac4471aec78f2f2e2138137e
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-05-30 12:35:37 +0400

    GO

--- custom_report/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Custom Invoice Report',
-    'version': '17.0.1.0.0',
-    'depends': ['base', 'account'],
-    'data': [
-        'report/report_template.xml',
-        'views/report_action.xml',
-    ],
-}
