b6c67fddc33180d10702d797f00ab356289293d0
Oliver <oliver@odoo4projects.com>
2025-06-06 18:28:25 -0300

    docu

---
+++ config/zehntech_customer_document_management/models/__init__.py
@@ -0,0 +1,17 @@
+from . import res_partner_document
+from . import res_config_settings
+from . import res_partner
+from . import signature_wizard
+from . import customer_document
+
+from . import document_dashboard
+from . import res_partner_document_tag
+
+from . import ir_attachment
+from . import res_partner_document_category
+
+
+
+
+
+
