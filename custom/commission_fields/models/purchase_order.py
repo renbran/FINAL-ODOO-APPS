fbe5956020e628a5315f86357b39d43e0b623ac9
ODOO4projects - Quick Git <support@ODOO4projects.com>
2025-06-10 09:58:47 +0000

    QUICK INSTALL

--- custom/commission_fields/models/purchase_order.py
+++ custom/commission_fields/models/purchase_order.py
@@ -6,13 +6,14 @@ class PurchaseOrder(models.Model):
 
     default_account_id = fields.Many2one('account.account', string='Default Expense Account',
         help='Default account to use for purchase order lines if not set on product or category.')
-
+    
     @api.model
     def create(self, vals):
         order = super().create(vals)
         for line in order.order_line:
             if not line.account_id:
-                default_account = order.default_account_id or order.company_id.expense_account_id
+                # Use default_account_id if set, otherwise fallback to company's default expense account (if exists)
+                default_account = order.default_account_id or getattr(order.company_id, 'expense_account_id', False)
                 if default_account:
                     line.account_id = default_account
         return order
@@ -27,7 +28,7 @@ class PurchaseOrderLine(models.Model):
     def create(self, vals):
         if not vals.get('account_id') and vals.get('order_id'):
             order = self.env['purchase.order'].browse(vals['order_id'])
-            default_account = order.default_account_id or order.company_id.expense_account_id
+            default_account = order.default_account_id or getattr(order.company_id, 'expense_account_id', False)
             if default_account:
                 vals['account_id'] = default_account.id
         return super().create(vals)
