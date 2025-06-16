979ffc2c05a10fe49a135742c0feb3b7cbb1b389
ODOO4projects - Quick Git <support@ODOO4projects.com>
2025-06-16 06:50:43 +0000

    QUICK INSTALL

--- custom/commission_fields/models/account_move.py
+++ custom/commission_fields/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    deal_id = fields.Many2one('sale.order', string='Deal ID', readonly=False, copy=False, help="Related Sale Order Deal ID")
+    deal_id = fields.Char(string='Deal ID', readonly=False, copy=False, help="Related Sale Order Deal ID")
     booking_date = fields.Date(string='Booking Date', readonly=False, copy=False, help="Related Sale Order Booking Date")
     buyer_id = fields.Many2one('res.partner', string='Buyer', readonly=False, copy=False, help="Related Sale Order Buyer")
     project_id = fields.Many2one(
@@ -42,30 +42,24 @@ class AccountMove(models.Model):
 
     @api.model
     def create(self, vals):
-        # Map deal information from sale.order to account.move (customer invoice) if invoice_origin is set
+        # Set deal_id on invoice to the deal_id string from sale.order
         if vals.get('invoice_origin'):
             sale_order = self.env['sale.order'].search([('name', '=', vals['invoice_origin'])], limit=1)
             if sale_order:
-                # Only map if this is a customer invoice
-                move_type = vals.get('move_type') or self._context.get('default_move_type')
-                if move_type == 'out_invoice':
-                    if sale_order.deal_id and hasattr(sale_order.deal_id, 'id'):
-                        vals['deal_id'] = sale_order.deal_id.id
-                    vals['booking_date'] = sale_order.booking_date
-                    vals['buyer_id'] = sale_order.buyer_id.id
-                    vals['project_id'] = sale_order.project_id.id
-                    vals['unit_id'] = sale_order.unit_id.id
-                    vals['sale_value'] = sale_order.sale_value
+                vals['deal_id'] = sale_order.deal_id  # Set as string
+                vals['booking_date'] = sale_order.booking_date
+                vals['buyer_id'] = sale_order.buyer_id.id
+                vals['project_id'] = sale_order.project_id.id
+                vals['unit_id'] = sale_order.unit_id.id
+                vals['sale_value'] = sale_order.sale_value
         return super().create(vals)
 
     def write(self, vals):
         for move in self:
-            # Only update deal info for customer invoices
             if move.move_type == 'out_invoice' and move.invoice_origin and not any(f in vals for f in ['deal_id', 'booking_date', 'buyer_id', 'project_id', 'unit_id', 'sale_value']):
                 sale_order = self.env['sale.order'].search([('name', '=', move.invoice_origin)], limit=1)
                 if sale_order:
-                    if sale_order.deal_id and hasattr(sale_order.deal_id, 'id'):
-                        vals['deal_id'] = sale_order.deal_id.id
+                    vals['deal_id'] = sale_order.deal_id  # Set as string
                     vals['booking_date'] = sale_order.booking_date
                     vals['buyer_id'] = sale_order.buyer_id.id
                     vals['project_id'] = sale_order.project_id.id
