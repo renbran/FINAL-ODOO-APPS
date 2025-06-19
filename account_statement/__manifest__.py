c1fae611412a29a6a46181b4be8f048315407262
BLUE PURPLE <renbranmadelo@gmail.com>
2025-06-20 01:48:00 +0400

    g

--- account_statement/__manifest__.py
+++ account_statement/__manifest__.py
@@ -23,6 +23,7 @@
         'security/ir.model.access.csv',
         'data/report_paperformat.xml',
         'views/account_statement_views.xml',
+        'views/account_statement_wizard_views.xml',
         'report/account_statement_report_action.xml',
         'report/account_statement_report_template.xml',
     ],
--- account_statement/models/account_statement.py
+++ account_statement/models/account_statement.py
@@ -3,7 +3,8 @@ from odoo import models, fields
 class AccountStatement(models.Model):
     _name = 'account.statement'
     _description = 'Account Statement'
+    _order = 'date desc, id desc'
 
-    name = fields.Char(string='Statement Name')
-    date = fields.Date(string='Date')
+    name = fields.Char(string='Statement Name', required=True)
+    date = fields.Date(string='Date', required=True, default=fields.Date.context_today)
     # ...add more fields as needed...
