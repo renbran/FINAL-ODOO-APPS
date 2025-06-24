99dcef1636c69ef3454d8e6a7143f51d033e302e
ODOO4projects - Quick Git <support@ODOO4projects.com>
2025-06-24 03:38:55 +0000

    QUICK INSTALL

--- custom/osus_invoice_report/__manifest__.py
+++ custom/osus_invoice_report/__manifest__.py
@@ -1,4 +1,5 @@
 {
+<<<<<<< HEAD
     'name': 'OSUS Invoice Report',
     'version': '17.0.1.0.0',
     'summary': 'Professional UAE Tax Invoice Reports for Real Estate Commission',
@@ -26,6 +27,20 @@
         'views/account_move_views.xml',
         'views/sale_order_views.xml',
         'views/report_invoice.xml',
+=======
+    'name': 'Custom Accounting Reports',
+    'version': '17.0.1.0.0',
+    'summary': 'Custom Invoice, Bill and Receipt Reports',
+    'description': 'UK-formatted documents with UAE receipt standard',
+    'category': 'Accounting/Accounting',
+    'author': 'Your Company',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+        'views/report_invoice.xml',
+        'views/report_bill.xml',
+        'views/report_receipt.xml',
+>>>>>>> 50419ea8 (QUICK INSTALL)
     ],
     'assets': {
         'web.report_assets_pdf': [
@@ -35,5 +50,8 @@
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
+<<<<<<< HEAD
     'auto_install': False,
+=======
+>>>>>>> 50419ea8 (QUICK INSTALL)
 }
--- custom/osus_invoice_report/models/__init__.py
+++ custom/osus_invoice_report/models/__init__.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 from . import custom_invoice
 from . import sale_order
+=======
+from . import models
+>>>>>>> 50419ea8 (QUICK INSTALL)
