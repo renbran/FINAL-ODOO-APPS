8f431e38723338ec983ad760c14dc68e8fba8bb8
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-06-25 12:39:09 +0400

    r

--- accounting_pdf_reports/wizard/aged_partner.py
+++ accounting_pdf_reports/wizard/aged_partner.py
@@ -25,12 +25,14 @@ class AccountAgedTrialBalance(models.TransientModel):
             raise UserError(_('You must set a period length greater than 0.'))
         if not data['form']['date_from']:
             raise UserError(_('You must set a start date.'))
+        
         start = data['form']['date_from']
+        data['form'].update({'partner_ids': self.partner_ids.ids})  # Ensure partner_ids is in form data
+        
         for i in range(5)[::-1]:
             stop = start - relativedelta(days=period_length - 1)
             res[str(i)] = {
-                'name': (i != 0 and (str((5 - (i + 1)) * period_length) + '-' + str((5 - i) * period_length)) or (
-                            '+' + str(4 * period_length))),
+                'name': (i != 0 and (str((5 - (i + 1)) * period_length) + '-' + str((5 - i) * period_length)) or ('+' + str(4 * period_length))),
                 'stop': start.strftime('%Y-%m-%d'),
                 'start': (i != 0 and stop.strftime('%Y-%m-%d') or False),
             }
