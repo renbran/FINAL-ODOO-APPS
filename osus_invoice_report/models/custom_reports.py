c1ad0e7788bc01dc021ac3843915676437f0b8d5
BLUE PURPLE <139387428+renbran@users.noreply.github.com>
2025-06-30 15:20:08 +0400

    g

--- osus_invoice_report/models/custom_reports.py
+++ osus_invoice_report/models/custom_reports.py
@@ -1,13 +1,28 @@
 from odoo import models
 
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_print_custom_invoice(self):
-        return self.env.ref('custom_reports.action_report_custom_invoice').report_action(self)
+        return self.env.ref('osus_invoice_report.action_report_custom_invoice').report_action(self)
 
     def action_print_custom_bill(self):
-        return self.env.ref('custom_reports.action_report_custom_bill').report_action(self)
+        return self.env.ref('osus_invoice_report.action_report_custom_bill').report_action(self)
 
     def action_print_custom_receipt(self):
-        return self.env.ref('custom_reports.action_report_custom_receipt').report_action(self)
+        return self.env.ref('osus_invoice_report.action_report_custom_receipt').report_action(self)
+
+
+class CustomInvoiceReport(models.AbstractModel):
+    _name = 'report.osus_invoice_report.report_custom_invoice'
+    _description = 'Custom Invoice Report'
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.move'].browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'account.move',
+            'docs': docs,
+            'data': data,
+        }
