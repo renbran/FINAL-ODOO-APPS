0feaeaebae0bf506671843a9a65c47a38fe4353e
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-03 22:14:56 +0400

    g

---
+++ custom_fields/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Fields',
+    'version': '1.0',
+    'depends': ['sale', 'account'],
+    'data': [
+        'views/sale_order_views.xml',
+        'views/account_move_views.xml',
+        'views/custom_fields_list_search_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
