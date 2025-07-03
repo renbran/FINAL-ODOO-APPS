1893d4670cd57482c18ef0bcaae5c99ffc2fe3b4
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-03 15:06:55 +0400

    go

--- custom_fields/models/account_move.py
+++ custom_fields/models/account_move.py
@@ -3,50 +3,66 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # Deal Information Fields - Professional naming convention with _id suffix for relational fields
     booking_date = fields.Date(
         string='Booking Date',
         tracking=True,
+        help="Date when the deal was booked"
     )
     
     developer_commission = fields.Float(
         string='Developer Commission',
         tracking=True,
+        digits=(16, 2),
+        help="Commission percentage for the developer"
     )
     
-    buyer = fields.Many2one(
+    buyer_id = fields.Many2one(
         'res.partner',
         string='Buyer',
         tracking=True,
+        help="Customer who is purchasing the property"
     )
     
-    deal_id = fields.Integer(
-        string='Deal ID',
+    deal_id = fields.Char(
+        string='Deal Reference',
         tracking=True,
+        help="Unique identifier for this deal"
     )
     
-    project = fields.Many2one(
+    project_id = fields.Many2one(
         'product.template',
         string='Project',
         tracking=True,
         domain=[('detailed_type', '=', 'service')],
+        help="Select the project for this deal"
     )
     
     sale_value = fields.Monetary(
         string='Sale Value',
         tracking=True,
         currency_field='currency_id',
+        help="Total value of the property sale"
     )
     
-    unit = fields.Many2one(
+    unit_id = fields.Many2one(
         'product.product',
         string='Unit',
         tracking=True,
+        help="Specific unit being sold"
     )
 
     amount_total_words = fields.Char(
         string='Amount in Words',
         compute='_compute_amount_total_words',
         store=True,
+    )
+    
+    sale_order_type = fields.Char(
+        string='Sale Order Type',
+        compute='_compute_sale_order_type',
+        store=True,
+        help="Sale order type copied from the originating sale order"
     )
 
     @api.depends('amount_total')
@@ -60,6 +76,21 @@ class AccountMove(models.Model):
                 record.amount_total_words = f"{amount_str} {currency_name}"
             else:
                 record.amount_total_words = ''
+
+    @api.depends('invoice_origin')
+    def _compute_sale_order_type(self):
+        """Compute sale order type from originating sale order"""
+        for record in self:
+            if record.invoice_origin and record.move_type in ['out_invoice', 'out_refund']:
+                sale_order = self.env['sale.order'].search([
+                    ('name', '=', record.invoice_origin)
+                ], limit=1)
+                if sale_order and sale_order.sale_order_type_id:
+                    record.sale_order_type = sale_order.sale_order_type_id.name
+                else:
+                    record.sale_order_type = ''
+            else:
+                record.sale_order_type = ''
 
     @api.model
     def create(self, vals):
@@ -75,19 +106,19 @@ class AccountMove(models.Model):
                 record.write({
                     'booking_date': sale_order.booking_date,
                     'developer_commission': sale_order.developer_commission,
-                    'buyer': sale_order.buyer_id.id if sale_order.buyer_id else False,
+                    'buyer_id': sale_order.buyer_id.id if sale_order.buyer_id else False,
                     'deal_id': sale_order.deal_id,
-                    'project': sale_order.project_template_id.id if sale_order.project_template_id else False,
+                    'project_id': sale_order.project_id.id if sale_order.project_id else False,
                     'sale_value': sale_order.sale_value,
-                    'unit': sale_order.unit_id.id if sale_order.unit_id else False,
+                    'unit_id': sale_order.unit_id.id if sale_order.unit_id else False,
                 })
         return record
 
-    @api.onchange('project')
-    def _onchange_project(self):
+    @api.onchange('project_id')
+    def _onchange_project_id(self):
         """Update unit domain when project changes"""
-        if self.project:
-            domain = [('product_tmpl_id', '=', self.project.id)]
-            return {'domain': {'unit': domain}}
+        if self.project_id:
+            domain = [('product_tmpl_id', '=', self.project_id.id)]
+            return {'domain': {'unit_id': domain}}
         else:
-            return {'domain': {'unit': []}}
+            return {'domain': {'unit_id': []}}
