28792cb1923557e250eec84cc25ba4f44ed75b7b
ODOO4projects - Quick Git <support@ODOO4projects.com>
2025-07-03 09:35:06 +0000

    QUICK INSTALL

---
+++ custom/custom_fields/models/deal_fields_mixin.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class DealFieldsMixin(models.AbstractModel):
+    _name = 'deal.fields.mixin'
+    _description = 'Deal Fields Mixin'
+
+    booking_date = fields.Date(string='Booking Date', tracking=True)
+    developer_commission = fields.Float(string='Broker Commission', tracking=True)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', tracking=True)
+    deal_id = fields.Integer(string='Deal ID', tracking=True)
+    project_id = fields.Many2one('product.template', string='Project Name', tracking=True)
+    sale_value = fields.Monetary(string='Sale Value', tracking=True, currency_field='currency_id')
+    unit_id = fields.Many2one('product.product', string='Unit', tracking=True, domain="[('product_tmpl_id', '=', project_id)]")
--- custom/custom_fields/models/sale_order.py
+++ custom/custom_fields/models/sale_order.py
@@ -1,43 +1,8 @@
 from odoo import models, fields, api
+from .deal_fields_mixin import DealFieldsMixin
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    # The field names in the model must match the view exactly
-    booking_date = fields.Date(
-        string='Booking Date',
-        tracking=True,
-    )
-    
-    developer_commission = fields.Float(
-        string='Broker Commission',
-        tracking=True,
-        digits=(16, 2),
-    )
-    
-    buyer_id = fields.Many2one(
-        'res.partner',
-        string='Buyer',
-        tracking=True,
-    )
-    
-    deal_id = fields.Integer(
-        string='Deal ID',
-        tracking=True,
-        copy=False,
-    )
-    
-    project_id = fields.Many2one(
-        'product.template',
-        string='Project Name',
-        tracking=True,
-    )
-    
-    sale_value = fields.Monetary(
-        string='Sale Value',
-        tracking=True,
-        currency_field='currency_id',
-    )
+    _inherit = ['sale.order', 'deal.fields.mixin']
 
     sale_value_formatted = fields.Char(
         string='Formatted Sale Value',
@@ -49,10 +14,3 @@ class SaleOrder(models.Model):
         from ..utils.number_format import format_amount
         for rec in self:
             rec.sale_value_formatted = format_amount(rec.sale_value)
-    
-    unit_id = fields.Many2one(
-        'product.product',
-        string='Unit',
-        tracking=True,
-        domain="[('product_tmpl_id', '=', project_id)]",
-    )
