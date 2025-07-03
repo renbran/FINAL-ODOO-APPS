2d54f34d4b8ae10fbb62a3ad1a7635505b18b614
ODOO4projects - Quick Git <support@ODOO4projects.com>
2025-07-03 03:38:56 +0000

    QUICK INSTALL

--- custom/osus_invoice_report/models/sale_order.py
+++ custom/osus_invoice_report/models/sale_order.py
@@ -27,10 +27,12 @@ class SaleOrder(models.Model):
         copy=False,
     )
     
-    project_id = fields.Many2one(
+    project_template_id = fields.Many2one(
         'product.template',
         string='Project Name',
         tracking=True,
+        domain=[('detailed_type', '=', 'service')],
+        help="Select a product template that represents the project"
     )
     
     sale_value = fields.Monetary(
@@ -43,4 +45,5 @@ class SaleOrder(models.Model):
         'product.product',
         string='Unit',
         tracking=True,
+        domain="[('product_tmpl_id', '=', project_template_id)]",
     )
