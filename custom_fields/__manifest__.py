860187ea98ebd189b596715669bc3e77c491604d
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-04 06:25:56 +0400

    Add cleanup script to remove old problematic view

--- custom_fields/__manifest__.py
+++ custom_fields/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['sale', 'account'],
     'data': [
+        'data/cleanup_old_views.xml',
         'views/sale_order_views.xml',
         'views/account_move_views.xml',
         'views/custom_fields_list_search_views.xml',
