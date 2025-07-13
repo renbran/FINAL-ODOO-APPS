0e71f98d4d55c57575e83de0353f137d8390cab5
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-13 08:10:29 +0400

    g

---
+++ osus_invoice_report/models/account_move.py
@@ -0,0 +1,11 @@
+from odoo import models, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.multi
+    def action_print_custom_invoice(self):
+        """
+        Print the PDF copy of the invoice using the custom report action.
+        """
+        return self.env.ref('osus_invoice_report.action_report_custom_invoice_modern').report_action(self)
