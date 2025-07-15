41b3b54decac39037c7583df1a9e972daf8ca683
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-16 01:04:56 +0400

    h

--- enterprise_dynamic_reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2025 OSUS
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
-from . import models
--- enterprise_dynamic_reports/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2025 OSUS
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
-from . import dashboard_main
-from . import financial_report_base
-from . import balance_sheet_report
-from . import profit_loss_report
