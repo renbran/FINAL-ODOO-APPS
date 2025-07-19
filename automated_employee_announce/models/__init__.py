07c52fd44109563c1d7cad1f3ba3626bb8216de9
CloudPepper Developer <renbranmadelo@gmail.com>
2025-07-20 00:04:18 +0400

    g

--- automated_employee_announce/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import automated_mail_rule
-from . import sale_order_mail_automation
-from . import hr_employee
--- custom_background/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-from . import report
-from . import res_company
-from . import report_background_lang
-from . import report_company_background_lang
