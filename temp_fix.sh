93c73f7202fb33898d47f3d360dfb29af669ec94
BLUE PURPLE <renbranmadelo@gmail.com>
2025-07-21 10:11:46 +0400

    g

--- temp_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Temporary directory environment fix
-export TMPDIR=/var/odoo/osuspro/temp
-export TMP=/var/odoo/osuspro/temp  
-export TEMP=/var/odoo/osuspro/temp
-mkdir -p /var/odoo/osuspro/temp
-chmod 755 /var/odoo/osuspro/temp
